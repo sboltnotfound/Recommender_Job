ddd4552b72f6d95623693d6a8b39a0c536543e50
sboltnotfound <148108300+sboltnotfound@users.noreply.github.com>
2025-09-03 07:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+import os
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from flask_cors import CORS
@@ -80,4 +81,5 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     
+
 
